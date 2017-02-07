bb1d97a3b28932d73875829accc4d48ef51338e2
MasayaOkawa <masamasa9841@gmail.com>
2017-02-08 08:31:42 +0900

    add joystick

--- scripts/map.py
+++ scripts/map.py
@@ -1,28 +1,49 @@
 #! /usr/bin/env python
 import rospy
+from PIL import Image
 from std_msgs.msg import Header
 from nav_msgs.msg import MapMetaData, OccupancyGrid
 from geometry_msgs.msg import Pose, Point, Quaternion
+from sensor_msgs.msg import Joy
 
 class map(object):
     def __init__(self):
-        rospy.init_node('map')
-        self.pub = rospy.Publisher('map', OccupancyGrid, queue_size=10)
+        rospy.init_node('tinpopo')
+        self.x = 0 
+        self.y = 0 
+        self.pub = rospy.Publisher('/uedaoniityann', OccupancyGrid, queue_size=1)
+        self.sub = rospy.Subscriber('joy', Joy, self.joy_callthrow, queue_size = 1)
         self.header = Header()
         self.pose = Pose()
         self.point = Point()
         self.quaternion = Quaternion()
         self.info = MapMetaData()
         self.bar = OccupancyGrid()
-        self.header.seq =0
+        self.header.seq = 0
+        im = Image.open("./tinpopo.jpg")
+        rgb_im = im.convert('RGB')
+        self.size = rgb_im.size
+        im2 = Image.new('RGBA', self.size)
+        self.hoge = []
+        for y in range(self.size[1]):
+            for x in range(self.size[0]):
+                r, g, b  = rgb_im.getpixel((x,y))
+                r = r * 100 / 255
+                self.hoge.append(r)
+    
+    def joy_callthrow(self, msg):
+        print msg
+        self.x -= msg.axes[3] * 0.1
+        self.y += msg.axes[4] * 0.1
+
     def run(self):
         while not rospy.is_shutdown():
             self.header.seq += 1
             self.header.stamp = rospy.Time.now()
-            self.header.frame_id = "map"
+            self.header.frame_id = 'map'
             
-            self.point.x = 0.0
-            self.point.y = 0.0
+            self.point.x = self.x 
+            self.point.y = self.y
             self.point.z = 0.0
 
             self.quaternion.x = 0.0
@@ -34,14 +55,14 @@ class map(object):
             self.pose.orientation = self.quaternion
 
             self.info.map_load_time = rospy.Time.now()
-            self.info.resolution = 0.05
-            self.info.width = 100
-            self.info.height = 100
+            self.info.resolution = 0.1
+            self.info.width = self.size[0]
+            self.info.height = self.size[1]
             self.info.origin = self.pose
 
             self.bar.info = self.info
-            self.a = [-1 for i in range (10000) ]
-            self.data = self.a
+            self.bar.header = self.header
+            self.bar.data = self.hoge
             self.pub.publish(self.bar)
 
 if __name__ == '__main__':
